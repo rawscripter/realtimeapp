61bc1283d5979a56cb670ae671f5c1acdea516cb
rawscripter <rawscripterx@gmail.com>
2019-03-30 22:36:22 +0600

    relatioships complete

--- app/User.php
+++ app/User.php
@@ -2,12 +2,12 @@
 
 namespace App;
 
+use App\Model\Question;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
-{
+class User extends Authenticatable {
     use Notifiable;
 
     /**
@@ -36,4 +36,9 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function question()
+    {
+        return $this->hasMany(Question::class);
+    }
 }
