4b63a93de6a2191f98e9c2448c29c92350691b0c
rawscripter <rawscripterx@gmail.com>
2019-03-31 18:11:07 +0600

    JWT routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+
 Route::apiResource('/question','QuestionController');
 
 Route::apiResource('/category','CategoryController');
@@ -7,4 +8,19 @@ Route::apiResource('/category','CategoryController');
 Route::apiResource('/question/{question}/reply','ReplyController');
 Route::post('/{reply}/like','LikeController@like');
 Route::delete('/{reply}/like','LikeController@disLike');
+
+
+
+Route::group([
+    'middleware' => 'api',
+    'prefix' => 'auth'
+], function ($router) {
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+
+});
+
+
 
